7dd4075d6da7bdfb9a687f8de89783fc902de9d8
Karishma0210 <42887239+Karishma0210@users.noreply.github.com>
2019-04-13 15:24:46 +0400

    Update server.java

--- server.java
+++ server.java
@@ -17,26 +17,26 @@ public class sever {
      */
 	public static void main(String[] args) throws IOException {
         
-        ServerSocket welcomeSocket = new ServerSocket(6789);
-        System.out.println("Server Ready!!!");
-        while(true){
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("Server-client joined!!!");
-            DataOutputStream strOut = new DataOutputStream(connectionSocket.getOutputStream());
-            
-            String line;        //for storing data to be read by BufferedReader
-            BufferedReader buffR = new BufferedReader(new
-                                        FileReader("resources/fuelPrice.txt")); //will read data from this path
-            ArrayList<String> fuelData = new ArrayList<>(); //for storing values of files
-            while ((line = buffR.readLine()) != null) {     //read line of file until line becomes null(go line by line in to file)
-                    fuelData.add(line); //add read single value to arraylist of fuelData
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+		System.out.println("Server Ready!!!");
+		while(true){
+			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("Server-client joined!!!");
+			DataOutputStream strOut = new DataOutputStream(connectionSocket.getOutputStream());
+
+			String line;        //for storing data to be read by BufferedReader
+			BufferedReader buffR = new BufferedReader(new
+						FileReader("resources/fuelPrice.txt")); //will read data from this path
+			ArrayList<String> fuelData = new ArrayList<>(); //for storing values of files
+			while ((line = buffR.readLine()) != null) {     //read line of file until line becomes null(go line by line in to file)
+				fuelData.add(line); //add read single value to arraylist of fuelData
+			}
+			strOut.writeUTF(String.valueOf(fuelData));
+			/*DataInputStream stringInp = new DataInputStream(new FileInputStream(connectionSocket.getOutputStream()));
+			String line;
+			while ((line = buffR.readLine()) != null) {     //read line of file until line becomes null(go line by line in to file)
+				fuelData.add(line); //add read single value to arraylist of fuelData
+			}*/
 		}
-		strOut.writeUTF(String.valueOf(fuelData));
-		/*DataInputStream stringInp = new DataInputStream(new FileInputStream(connectionSocket.getOutputStream()));
-		String line;
-		while ((line = buffR.readLine()) != null) {     //read line of file until line becomes null(go line by line in to file)
-			fuelData.add(line); //add read single value to arraylist of fuelData
-		}*/
-            
 	}
 }
